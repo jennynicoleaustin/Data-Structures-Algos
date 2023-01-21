3e027f8eb8a2415c08d023d707d6e0ce4a686435
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-01-20 20:29:11 -0600

    lonelyInt

--- Java Prep Work /src/main/java/org/example/HackerRank.java
+++ Java Prep Work /src/main/java/org/example/HackerRank.java
@@ -5,15 +5,15 @@ import java.util.List;
 
 public class HackerRank {
 
-//    s = time in 12 hour format '12:01:00PM'
-//    convert s to 24 hour format '12:01:00'
+    //    s = time in 12 hour format '12:01:00PM'
+    //    convert s to 24 hour format '12:01:00'
     public static String timeConversion(String s) {
-//        if s ends with PM  & not 12 -> add 12 hours
-//        if s ends with AM & not 12 -> report as is, if 12 -> 00:00
+        //        if s ends with PM  & not 12 -> add 12 hours
+        //        if s ends with AM & not 12 -> report as is, if 12 -> 00:00
 
-        String hourTime = s.substring(0,2);
+        String hourTime = s.substring(0, 2);
         String restTime = s.substring(2, 8);
-        String aOrP = s.substring(8,10);
+        String aOrP = s.substring(8, 10);
         String time = "";
 
         if (hourTime.equals("12") && aOrP.equals("AM")) {
@@ -33,15 +33,22 @@ public class HackerRank {
         return time;
     }
 
-//Given a list of numbers with an odd number find the median.
+    //Given a list of numbers with an odd number find the median.
     public static int findMedian(List<Integer> arr) {
         // Write your code here
-    int size = arr.size();
-    int halfNoRemainder = size /2;
+        int size = arr.size();
+        int halfNoRemainder = size / 2;
         Collections.sort(arr);
         int median = arr.get(halfNoRemainder);
         System.out.println(median);
-    return median;
+        return median;
+    }
+
+    //    Give an array of integers where all elements except one occur 2x -> isolate the element that only occurs once.
+    public static int lonelyinteger(List<Integer> a) {
+        //    if itemA == itemB -> remove itemA and remove itemB
+
+    return a.get(0);
     }
 
 }
--- Java Prep Work /src/main/java/org/example/Main.java
+++ Java Prep Work /src/main/java/org/example/Main.java
@@ -3,16 +3,17 @@ package org.example;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.example.HackerRank.findMedian;
+import static org.example.HackerRank.lonelyinteger;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> integerList = Arrays.asList(2, 9, 3, 4, 5);
+        List<Integer> integerList = Arrays.asList(2, 2, 9, 9, 3, 8, 3, 4, 4);
         //        plusMinus(integerList);
         //        miniMaxSum(integerList);
 //        timeConversion("12:02:00PM");
+//        findMedian(integerList);
+        lonelyinteger(integerList);
 
-        findMedian(integerList);
     } // Main Method close
 }
