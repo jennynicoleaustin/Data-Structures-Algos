ce1ae99aa473204b37376a3438862c0d53eb0cd5
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-01-21 12:28:44 -0600

    udemy example questions

---
+++ Hacker+Leet/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> integerList = Arrays.asList(1,1,2);
+        //        plusMinus(integerList);
+        //        miniMaxSum(integerList);
+//        timeConversion("12:02:00PM");
+//        findMedian(integerList);
+//        lonelyinteger(integerList);
+
+    } // Main Method close
+}
