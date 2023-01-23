2b0979510eb6af42224b62f3eaa70bef6576f3f0
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-01-22 19:25:24 -0600

    Section 6: Stacks & Queues V 43 Stack: Constructor

--- udemy-interview-prep/DataStructuresAlgos/src/main/java/org/example/S6StacksAndQueues/Stack.java
+++ udemy-interview-prep/DataStructuresAlgos/src/main/java/org/example/S6StacksAndQueues/Stack.java
@@ -1,8 +1,25 @@
 package org.example.S6StacksAndQueues;
 import org.w3c.dom.Node;
 
-
 public class Stack {
+    class Node {
+        int value;
+        int next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node top; // equivalent to head in a linked list
+    private int height; // equivalent to length in linked list
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
     public void printStack() {
         Node temp = top;
         while (temp != null) {
