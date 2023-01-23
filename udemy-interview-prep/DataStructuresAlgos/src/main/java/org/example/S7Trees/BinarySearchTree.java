22d1ef068e68761b078d4c73145aeb51d0c97c7a
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-01-22 20:42:20 -0600

    Section 7: Trees V 53: Constructor

--- udemy-interview-prep/DataStructuresAlgos/src/main/java/org/example/S7Trees/BinarySearchTree.java
+++ udemy-interview-prep/DataStructuresAlgos/src/main/java/org/example/S7Trees/BinarySearchTree.java
@@ -2,5 +2,19 @@ package org.example.S7Trees;
 
 public class BinarySearchTree {
 
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node (int value) {
+            this.value = value;
+        }
+    }
+
+    Node root ;
+
+
+
 
 }
---
+++ udemy-interview-prep/DataStructuresAlgos/src/main/java/org/example/S7Trees/Main.java
@@ -0,0 +1,12 @@
+package org.example.S7Trees;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        System.out.println("Root = " + myBST.root);
+
+    }
+}
