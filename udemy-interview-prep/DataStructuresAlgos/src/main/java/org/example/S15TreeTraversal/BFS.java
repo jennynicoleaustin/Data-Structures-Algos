b8ff89499049d44819eb5de9f9383402aa6be729
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-01-24 11:03:11 -0600

    Section 15: Tree Traversal -> BFS

--- udemy-interview-prep/DataStructuresAlgos/src/main/java/org/example/S15TreeTraversal/BFS.java
+++ udemy-interview-prep/DataStructuresAlgos/src/main/java/org/example/S15TreeTraversal/BFS.java
@@ -1,10 +1,8 @@
 package org.example.S15TreeTraversal;
-import org.w3c.dom.Node;
-import java.util.ArrayList;
 
 public class BFS {
 
-
+// See BFS method in binary search tree class
 
 
 } // Breadth First Search
